4221b8ef0213638e9acef888755fdeaf51483812
senta <sentaro118@gmail.com>
2019-11-01 21:49:06 +0900

    modify Checkout components

--- src/functions/index.js
+++ src/functions/index.js
@@ -7,3 +7,45 @@ exports.handler = async (event, context) => {
     }),
   }
 }
+
+// require("dotenv").config()
+// var stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+
+// exports.handler = (event, context, callback) => {
+//   const requestBody = JSON.parse(event.body)
+//   const token = requestBody.token.id
+//   const amount = requestBody.charge.amount
+//   const currency = requestBody.charge.currency
+//   const email = requestBody.charge.email
+
+//   return stripe.charges
+//     .create({
+//       // Create Stripe charge with token
+//       amount,
+//       currency,
+//       receipt_email: email,
+//       description: "Serverless Stripe Test charge",
+//       source: token,
+//     })
+//     .then(charge => {
+//       // Success response
+//       const response = {
+//         statusCode: 200,
+//         body: JSON.stringify({
+//           message: `Charge processed succesfully!`,
+//           charge,
+//         }),
+//       }
+//       callback(null, response)
+//     })
+//     .catch(err => {
+//       // Error response
+//       const response = {
+//         statusCode: 500,
+//         body: JSON.stringify({
+//           error: err.message,
+//         }),
+//       }
+//       callback(null, response)
+//     })
+// }
