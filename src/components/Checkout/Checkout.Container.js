8b068275e96d1044e785118050737644d37d2cf3
senta <sentaro118@gmail.com>
2019-11-02 19:22:11 +0900

    set ssr for stripe

--- src/components/Checkout/Checkout.Container.js
+++ src/components/Checkout/Checkout.Container.js
@@ -1,12 +1,23 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Elements, StripeProvider } from "react-stripe-elements"
 
 import CheckoutForm from "@components/Checkout/Checkout.Form"
 import StripeHookProvider from "@components/Checkout/StripeHookProvider"
 
 const CheckoutContainer = () => {
+  const [stripe, setStripe] = useState(null)
+
+  useEffect(() => {
+    if (window.Stripe) {
+      setStripe(window.Stripe("pk_test_1Y4LgFWhiJPEsivDjXsJA8cJ00tdvDj2SX"))
+    } else {
+      document.querySelector("#stripe-js").addEventListener("load", () => {
+        setStripe(window.Stripe("pk_test_1Y4LgFWhiJPEsivDjXsJA8cJ00tdvDj2SX"))
+      })
+    }
+  }, [])
   return (
-    <StripeProvider apiKey="pk_test_1Y4LgFWhiJPEsivDjXsJA8cJ00tdvDj2SX">
+    <StripeProvider stripe={stripe}>
       <Elements>
         <StripeHookProvider>
           <CheckoutForm />
